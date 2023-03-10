8af0a562580d70433a928ff62ec5f2c3cb15d6d5
kota-shiokara <ikanoshiokara.fun@gmail.com>
2023-03-11 00:26:07 +0900

    fix gradle version

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "jp.ikanoshiokara"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 kotlin {
     android()
--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "jp.ikanoshiokara"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 
 kotlin {
